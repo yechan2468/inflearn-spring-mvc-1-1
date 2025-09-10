041a353ddb3de9f0c0ba7dc5754d394f5b695953
yechan <yechan24680@gmail.com>
2025-09-10 15:05:30 +0900

    section7 52

---
+++ src/main/java/yechan/inflearn_spring_mvc_1_1/basic/HelloData.java
@@ -0,0 +1,10 @@
+package yechan.inflearn_spring_mvc_1_1.basic;
+
+import lombok.Data;
+
+@Data
+public class HelloData {
+
+    private String username;
+    private Integer age;
+}
--- src/main/java/yechan/inflearn_spring_mvc_1_1/basic/request/RequestParamController.java
+++ src/main/java/yechan/inflearn_spring_mvc_1_1/basic/request/RequestParamController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import yechan.inflearn_spring_mvc_1_1.basic.HelloData;
 
 import java.io.IOException;
 import java.util.Map;
@@ -83,6 +84,20 @@ public class RequestParamController {
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return "ok";
     }
 }
